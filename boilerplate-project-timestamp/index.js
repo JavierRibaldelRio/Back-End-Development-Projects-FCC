3a0b72a76e2c2c34e4cad0246713d19187a29b78
JavierRibaldelRio <javierribal@gmail.com>
2022-12-04 22:54:30 +0100

    Proyecto 1

---
+++ boilerplate-project-timestamp/index.js
@@ -0,0 +1,42 @@
+// index.js
+// where your node app starts
+
+// init project
+
+var PORT = process.env.PORT | 5000;
+
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+//Creación de la nueva ruta
+app.get("/api/:date", (req, res) => {
+
+
+
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
