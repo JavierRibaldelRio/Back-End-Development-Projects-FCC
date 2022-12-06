f939133d627f8ac1f3520b785ae1d9ded38768ff
JavierRibaldelRio <javierribal@gmail.com>
2022-12-06 17:13:47 +0100

    He acabado la segunda tarea

---
+++ boilerplate-project-headerparser/index.js
@@ -0,0 +1,36 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+
+  const respuesta = { ipaddress: req.socket.remoteAddress, language: req.acceptsLanguages(), software: req.get('user-agent') }
+  res.json(respuesta)
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
