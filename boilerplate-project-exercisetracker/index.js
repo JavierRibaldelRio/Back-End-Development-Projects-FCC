9e1bdaa1072541d1b120a181038b56cbea2baf78
JavierRibaldelRio <javierribal@gmail.com>
2022-12-07 23:51:09 +0100

    He empezado a programar

--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -3,6 +3,11 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const Usuario = require('./models/Usuarios');
+
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -10,6 +15,67 @@ app.get('/', (req, res) => {
 });
 
 
+app.post('/api/users', (req, res) => {
+
+  //Coge el nombre del usuario
+  const username = req.body.username;
+
+  //Crea el nuevo usuario con solo con el nombre
+  var nuevoUsuario = new Usuario({ username: username, count: 0 });
+
+  //Guarda el usuario
+  nuevoUsuario.save();
+
+  //Devuelve el id y el nombre de usuario y pone el contador a cerop
+  res.json({ username: nuevoUsuario.username, _id: nuevoUsuario._id });
+});
+
+app.get('/api/users', (req, res) => {
+
+  Usuario.find({})
+    .sort({ username: 1 })
+    .exec((err, data) => {
+
+      if (err) {
+
+        console.log('err :>> ', err);
+      }
+
+      var respuesta = []
+
+      respuesta = data.map((x) => { return { username: x.username, _id: x._id } })
+
+      res.json(respuesta);
+    });
+
+});
+
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+
+  const id = req.params._id;
+
+  console.log('bosy :>> ', req.body);
+
+  var { description, duration, date } = req.body;
+
+  console.log('description :>> ', description);
+  console.log('duration :>> ', duration);
+  console.log('data :>> ', date);
+
+  Usuario.findById(id, (err, data) => {
+
+    if (err) {
+      console.log('err :>> ', err);
+    }
+
+    data.logs.push()
+
+
+  })
+
+
+});
 
 
 
---
+++ boilerplate-project-exercisetracker/models/Usuarios.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+
+
+var usuario = new mongoose.Schema({
+
+    username: String,
+    description: String,
+    duration: Number,
+    date: String,
+    count: { type: Number, default: 0 },
+    log: [{
+        description: String,
+        duration: Number,
+        date: String
+    }]
+});
+
+mongoose.connect(process.env.URI);
+
+module.exports = mongoose.model('Usuario', usuario);
