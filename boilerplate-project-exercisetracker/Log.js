0eb7c68fd344c00cc43a3c1a0b3ca0c0d0cb80b5
JavierRibaldelRio <javierribal@gmail.com>
2022-12-08 17:08:13 +0100

    He acanzado

---
+++ boilerplate-project-exercisetracker/Log.js
@@ -0,0 +1,13 @@
+class Log {
+
+    constructor(description, duration, date) {
+
+
+        this.description = description;
+        this.duration = duration;
+        this.date = date;
+    }
+
+};
+
+module.exports = Log;
--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -3,7 +3,12 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// Importa el usuario
 const Usuario = require('./models/Usuarios');
+
+//Importa el objeto LOG
+
+var Log = require('./Log');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,19 +35,24 @@ app.post('/api/users', (req, res) => {
   res.json({ username: nuevoUsuario.username, _id: nuevoUsuario._id });
 });
 
+//Muestra todos los usuarios
 app.get('/api/users', (req, res) => {
 
+  //Coge todos los usuarios
   Usuario.find({})
-    .sort({ username: 1 })
+    .sort({ username: 1 })    //Los ordena
     .exec((err, data) => {
 
+      //Si hay un error lo muesta
       if (err) {
-
         console.log('err :>> ', err);
+        res.send(err)
       }
 
+      //Almacena la respuesta
       var respuesta = []
 
+      //Formatea la respuesta correctamente
       respuesta = data.map((x) => { return { username: x.username, _id: x._id } })
 
       res.json(respuesta);
@@ -50,34 +60,69 @@ app.get('/api/users', (req, res) => {
 
 });
 
-
+//Añade un ejercicio a un usuario
 app.post('/api/users/:_id/exercises', (req, res) => {
 
+  //Obtine id de URL
   const id = req.params._id;
 
-  console.log('bosy :>> ', req.body);
-
+  //Extrae formulario descripcion duracion fecha
   var { description, duration, date } = req.body;
 
-  console.log('description :>> ', description);
-  console.log('duration :>> ', duration);
-  console.log('data :>> ', date);
+  //Pasa el string a fecha
+  date = new Date(date);
+
+  //Convierte la fecha en el string de forma deseade
+  date = date.toDateString();
+
+  //Busca el usuario
+  Usuario.findById(id, (err, data) => {
+
+    //Si hay un error lo muestra
+    if (err) {
+      console.log('err :>> ', err);
+      res.send(err);
+    }
+
+    else {
+
+      //Crea un nuevo log
+      var logNuevo = new Log(description, duration, date);
+
+      //Añade al array el nuevo log
+      data.log.push(logNuevo);
+
+      //Aumenta en uno el contador de ejercicios
+      data.count++;
+
+      //guarda los cambios
+      data.save();
+
+      //Define la respuesta
+      const respuesta = { username: data.username, description: description, duration: duration, date: date, _id: data._id };
+
+      //Envia la respuesta
+      res.json(respuesta);
+    }
+  });
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+
+  const id = req.params._id;
 
   Usuario.findById(id, (err, data) => {
 
     if (err) {
       console.log('err :>> ', err);
+      res.send(err);
     }
 
-    data.logs.push()
-
-
+    else {
+      res.send(data);
+    }
   })
-
-
-});
-
-
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
